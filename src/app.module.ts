18bb22665d4b0a7e9f998dce12d6086c1a79821a
Prestige Nsien <prestigensien45@gmail.com>
2026-02-15 09:44:55 +0100

    new changes to postgre config in app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -19,14 +19,13 @@ import { ThrottlerModule } from '@nestjs/throttler';
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'postgres', 
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT),
-      username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false,
+      },
       autoLoadEntities: true,
-      synchronize: true, 
+      synchronize: true,
     }),
 
     CacheModule.register({
